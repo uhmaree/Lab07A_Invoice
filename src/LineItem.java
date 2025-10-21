7f960be83affbc8ccd21628049069f106ec12f30
uhmaree <amarebrooks4@gmail.com>
2025-10-20 22:59:30 -0400

    LineItem.java

--- src/LineItem.java
+++ src/LineItem.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class LineItem {
+    private Product product;
+    private int quantity;
+
+    public LineItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public double getSubtotal() {
+        return product.getUnitPrice() * quantity;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-15s %5d %10.2f %10.2f",
+                product.getName(), quantity, product.getUnitPrice(), getSubtotal());
+    }
 }
