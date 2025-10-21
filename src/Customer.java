66da837669dc5bc3a952e10f648dec2348107cf5
uhmaree <amarebrooks4@gmail.com>
2025-10-20 23:00:12 -0400

    Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Customer {
+    private String name;
+    private String address;
+
+    public Customer(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return name + "\n" + address;
+    }
 }
