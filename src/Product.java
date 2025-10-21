466a4209029f1d5e76bf2f63fa9596333cfc94f4
uhmaree <amarebrooks4@gmail.com>
2025-10-20 22:59:18 -0400

    Product.java

--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Product {
+    private String name;
+    private double unitPrice;
+
+    public Product(String name, double unitPrice) {
+        this.name = name;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getName() { return name; }
+    public double getUnitPrice() { return unitPrice; }
 }
