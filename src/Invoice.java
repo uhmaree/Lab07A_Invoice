55ef9d61167920f51f856da91374569c621f1404
uhmaree <amarebrooks4@gmail.com>
2025-10-20 22:59:59 -0400

    Invoice.java

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Invoice {
+    private Customer customer;
+    private ArrayList<LineItem> items = new ArrayList<>();
+
+    public Invoice(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void addItem(LineItem item) {
+        items.add(item);
+    }
+
+    public double getTotal() {
+        return items.stream().mapToDouble(LineItem::getSubtotal).sum();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("===== INVOICE =====\n");
+        sb.append(customer.toString()).append("\n\n");
+        sb.append(String.format("%-15s %5s %10s %10s\n", "Item", "Qty", "Price", "Total"));
+        sb.append("---------------------------------------------------\n");
+        for (LineItem item : items) {
+            sb.append(item.toString()).append("\n");
+        }
+        sb.append("---------------------------------------------------\n");
+        sb.append(String.format("AMOUNT DUE: $%.2f\n", getTotal()));
+        return sb.toString();
+    }
 }
